b0c26aa406491a74e441ae2b17c3bf794e7fdf6f
josuenm <direct.josue@gmail.com>
2022-02-06 01:05:40 -0300

    Dev archives

---
+++ src/Tmdb.js
@@ -0,0 +1,108 @@
+const API_KEY = "11958cdf857be9935ae13761af946fac";
+const API_BASE = "https://api.themoviedb.org/3";
+
+/*
+- originais da netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação
+- comédia
+- terror
+- romance
+- documentários
+*/
+
+const basicFetch = async (endpoint) => {
+  const res = await fetch(`${API_BASE}${endpoint}`);
+  const json = await res.json();
+  return json;
+};
+
+const Tmdb = {
+  getHomeList: async () => {
+    return [
+      {
+        slug: "originals",
+        title: "Originais do Netflix",
+        items: await basicFetch(
+          `/discover/tv?with_network=213&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "trendings",
+        title: "Recomendados para Você",
+        items: await basicFetch(
+          `/trending/all/week?&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "toprated",
+        title: "Em Alta",
+        items: await basicFetch(
+          `/movie/top_rated?&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "action",
+        title: "Ação",
+        items: await basicFetch(
+          `/discover/movie?with_genres=28&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "comedy",
+        title: "Comédia",
+        items: await basicFetch(
+          `/discover/movie?with_genres=35&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "horror",
+        title: "Terror",
+        items: await basicFetch(
+          `/discover/movie?with_genres=27&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "romance",
+        title: "Romance",
+        items: await basicFetch(
+          `/discover/movie?with_genres=10749&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "documentary",
+        title: "Documentários",
+        items: await basicFetch(
+          `/discover/movie?with_genres=99&language=pt=BR&api_key=${API_KEY}`
+        ),
+      },
+    ];
+  },
+
+  getContentInfo: async (contentId, type) => {
+    let info = {};
+
+    if (contentId) {
+      switch (type) {
+        case "movie":
+          info = await basicFetch(
+            `/movie/${contentId}?language=pt-BR&api_key=${API_KEY}`
+          );
+          break;
+
+        case "tv":
+          info = await basicFetch(
+            `/tv/${contentId}?language=pt-BR&api_key=${API_KEY}`
+          );
+          break;
+
+        default:
+      }
+    }
+
+    return info;
+  },
+};
+
+export default Tmdb;
---
+++ src/components/FeaturedContent.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styles from "./FeaturedContent.module.css";
+
+const FeaturedContent = ({ item }) => {
+  let firstDate = new Date(item.first_air_date);
+  let genres = [];
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
+
+  let description = item.overview;
+  if (description.length < 1) {
+    description =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum tincidunt lectus eget mollis. Integer mauris ligula, sagittis accumsan libero sed, rutrum mattis mi. Vestibulum consequat, mauris quis maximus mollis, neque tellus scelerisque risus, nec tincidunt leo tellus iaculis eros.";
+  }
+  if (description.length > 200) {
+    description = description.substring(0, 200) + "...";
+  }
+
+  return (
+    <section
+      className={styles.featured}
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div className={styles.featured_vertical}>
+        <div
+          className={styles.featured_horizontal}
+          style={
+            item.original_name === "理想之城" ||
+            item.original_name === "亲爱的爸妈"
+              ? { paddingBottom: "150px" }
+              : null
+          }
+        >
+          <div className={styles.featured_name}>{item.original_name}</div>
+
+          {/* INFO */}
+          <div className={styles.featured_info}>
+            <div className={styles.featured_points}>
+              {item.vote_average} pontos
+            </div>
+            <div className={styles.featured_year}>
+              {firstDate.getFullYear()}
+            </div>
+            <div className={styles.featured_seasons}>
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons > 1 ? "s" : ""}
+            </div>
+          </div>
+
+          {/* DESCRIPTION */}
+          <div className={styles.featured_description}>{description}</div>
+
+          {/* BUTTONS */}
+          <div className={styles.featured_buttons}>
+            <a
+              href={`watch/${item.id}`}
+              className={styles.featured_watchButton}
+            >
+              ▶ Assistir
+            </a>
+            <a
+              href={`list/add/${item.id}`}
+              className={styles.featured_myListButton}
+            >
+              ✚ Minha Lista
+            </a>
+          </div>
+
+          <div className={styles.featured_genres}>
+            <strong>Genêro:</strong> {genres.join(", ")}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedContent;
